0d24d33d780344866d55e3639111a57ac83c1632
ankit <ankitray0308@gmail.com>
2024-02-05 19:37:45 +0530

    cors

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
+const cors = require('cors');
+
 
 const portFolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/portfolio",portFolioRoute);
 
