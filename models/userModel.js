7f90d79414572744394ede5aac50f7de9720aad1
ankit <ankitray0308@gmail.com>
2024-02-05 18:40:51 +0530

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        unique:true
+    }
+});
+
+module.exports = mongoose.model("users",userSchema);
